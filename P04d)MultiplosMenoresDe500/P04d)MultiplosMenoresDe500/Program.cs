1fe63b3d80d1bd3727c012e43cf605eb389fa9b8
selumoreno94 <selumoreno1994@gmail.com>
2017-03-21 10:47:57 +0100

    PRACTICA 1

---
+++ P04d)MultiplosMenoresDe500/P04d)MultiplosMenoresDe500/Program.cs
@@ -0,0 +1,60 @@
+﻿/* P04d)MultiplosMenoresDe500: 
+ Construir este método que:
+•	Recibe: Un enteros num.
+•	Hace: Presenta los múltiplos de num menores de 500.
+•	Devuelve: Nada.
+
+Prueba del método
+En el programa se pedirá un número de dos cifras y se llamará a este método, 
+pasándole dicho número.
+
+*/
+
+using System;
+
+class Program
+{
+    static int CapturaEntero(string texto, int min, int max)
+    {
+        int num;
+        bool esCorrecto;
+        do
+        {
+            Console.Write("\n\t {0} [{1}..{2}]: ", texto, min, max);
+            esCorrecto = Int32.TryParse(Console.ReadLine(), out num);
+            if (!esCorrecto)
+                Console.WriteLine("\n\t ** ERROR de FORMATO **");
+            else if (num < min || num > max)
+            {
+                Console.WriteLine("\n\t ** ERROR: VALOR FUERA DE RANGO **");
+                esCorrecto = false;
+            }
+        } while (!esCorrecto);
+
+        return num;
+    }
+
+    static void MultiplosMenoresDe500(int n)
+    {
+        int mult = n; // <-- El primer múltiplo.
+        Console.WriteLine("\n");
+        while (mult < 500)  // Mientras el múltiplo sea menor de 500
+        {
+            Console.Write("{0}\t", mult);   // <-- Escribo el múltiplo
+            mult = mult + n;              // <-- Obtengo el siguiente
+        }
+    }
+    static void Main(string[] args)
+    {
+        int num;
+        num = CapturaEntero("Dime el número al que encontrar sus múltiplos: ", 10, 99);
+
+        // la llamada al método
+        MultiplosMenoresDe500(num);
+
+
+        Console.Write("\n\n\n\t\t\t\tPulsa Intro para salir");
+        Console.ReadLine();
+
+    }
+}
---
+++ P21g)  MultiplicarSumando/P21g)  MultiplicarSumando/Program.cs
@@ -0,0 +1,66 @@
+﻿/* P21g) 	MultiplicarSumando: 
+1)	Recibe: Dos enteros a, b.
+2)	Hace: Calcula el producto de los dos, sin utilizar la operación *, sabiendo que a * b es sumar la a “b veces”.
+3)	Devuelve: El producto calculado.
+En el programa se pedirá dos números [0..10000) y llamando al método, mostrará el producto de estos dos números.
+Avanzado: Optimizarlo para que dé las menos vueltas posibles.
+
+*/
+
+using System;
+
+class Program
+{
+    static int CapturaEntero(string texto, int min, int max)
+    {
+        int num;
+        bool esCorrecto;
+        do
+        {
+            Console.Write("\n\t {0} [{1}..{2}]: ", texto, min, max);
+            esCorrecto = Int32.TryParse(Console.ReadLine(), out num);
+            if (!esCorrecto)
+                Console.WriteLine("\n\t ** ERROR de FORMATO **");
+            else if (num < min || num > max)
+            {
+                Console.WriteLine("\n\t ** ERROR: VALOR FUERA DE RANGO **");
+                esCorrecto = false;
+            }
+        } while (!esCorrecto);
+
+        return num;
+    }
+
+    static int MultiplicarSumando(int a, int b)
+    {
+        int producto = 0;
+        // Para que dé las menos vueltas posibles, si b>a intercambio sus valores
+        if (b > a)
+        {
+            int aux = a;
+            a = b;
+            b = aux;
+        }
+
+        // Cuando llegamos aquí b es menor o igual que a
+        for (int i = 0; i < b; i++)
+        {
+            producto = producto + a;
+        }
+
+        return producto;
+    }
+
+    static void Main(string[] args)
+    {
+        int num1, num2;
+        num1 = CapturaEntero("Dime el primer  factor: ", 0, 9999);
+        num2 = CapturaEntero("Dime el segundo factor: ", 0, 9999);
+        // la llamada al método, directamente en la presentación (podíamos haber guardado el resultado en una variable y mostrarla)
+        Console.WriteLine("\n\n\tEl producto de {0} x {1} = {2}", num1, num2, MultiplicarSumando(num1, num2));
+
+        Console.Write("\n\n\n\t\t\t\tPulsa Intro para salir");
+        Console.ReadLine();
+
+    }
+}
